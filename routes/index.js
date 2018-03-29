fd6ba39a91f609ffdbebc02f3556ec6e503107cc
crimson-med <mederic.burlet@gmail.com>
2018-03-30 03:58:46 +0800

    working algorithm

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+var fs = require('fs');
 var origin = 88212247;
-var map = {id:88212247, x:0, y:0, topNeighbourId:00000001, bottomNeighbourId:00000002,leftNeighbourId:00000003, rightNeighbourId:00000004 background: ''};
+var map = {id:88212247, x:0, y:0, topNeighbourId:00000001, bottomNeighbourId:00000002,leftNeighbourId:00000003, rightNeighbourId:00000004, background: ''};
 var NSLine = [];
 var Corigin = [0,0];
 
@@ -19,20 +20,23 @@ router.get('/', function(req, res, next) {
     nexter = response.data.leftNeighbourId;
     const myimage = "https://ankama.akamaized.net/games/dofus-tablette/assets/2.18.3/backgrounds/"+response.data.id+".jpg";
     var mapdata = {id: origin, x:0, y:0, topNeighbourId:response.data.topNeighbourId, bottomNeighbourId: response.data.bottomNeighbourId, leftNeighbourId:response.data.leftNeighbourId, rightNeighbourId:response.data.rightNeighbourId, background:myimage};
-    console.log("#1");
     visitedNodes.push(mapdata);
+    console.log("visitedNodes: "+visitedNodes[0].id);
     var toVisit = [
-      {id: topNeighbourId, x:mapdata.x, y:mapdata.y-1},
-      {id: bottomNeighbourId, x:mapdata.x, y:mapdata.y+1},
-      {id: rightNeighbourId, x:mapdata.x+1, y:mapdata.y}
+      {id: response.data.topNeighbourId, x:mapdata.x, y:mapdata.y-1},
+      {id: response.data.bottomNeighbourId, x:mapdata.x, y:mapdata.y+1},
+      {id: response.data.rightNeighbourId, x:mapdata.x+1, y:mapdata.y},
     ];
-    tovisitNodes.push(toVisit);
-    currentNode = {id: leftNeighbourId, x:mapdata.x-1, y:mapdata.y};
+    console.log("To visit: "+toVisit.length);
+    checkAndAddArr(toVisit);
+    // ADD IF NO DUPLICATES
+    console.log("visitedNodes: "+visitedNodes.length+"  ---  tovisitNodes: "+tovisitNodes.length);
+    currentNode = {id: response.data.leftNeighbourId, x:mapdata.x-1, y:mapdata.y};
     previousNode = mapdata;
     pathMapping(res);
   })
   .catch(error => {
-    //console.log(error);
+    console.log(error);
   });
 });
 
@@ -40,24 +44,37 @@ async function pathMapping(res) {
     while (tovisitNodes.length != 0) {
       await visitNode();
     }
-    render('index', { title: 'Dofus Discovery', json: NSLine});
+    for(const nodeNow of toVisitNodes){
+      console.log(nodeNow.id);
+    }
+    fs.writeFile('maps.txt', JSON.stringify(visitedNodes), function (err) {
+    if (err) throw err;
+      console.log('It\'s saved! in same location.');
+    });
+    res.render('index', { title: 'Dofus Discovery', json: visitedNodes});
 }
 function visitNode(){
   return new Promise((resolve, reject) => {
     axios.get('https://ankama.akamaized.net/games/dofus-tablette/assets/2.18.3/maps/'+currentNode.id+'.json')
     .then(response => {
-      visitedNodes.push(previousNode);
+      console.log("Trying to get: "+currentNode.id);
       const myimage = "https://ankama.akamaized.net/games/dofus-tablette/assets/2.18.3/backgrounds/"+response.data.id+".jpg";
       var mapdata = {id: currentNode.id, x:currentNode.x, y:currentNode.y, topNeighbourId:response.data.topNeighbourId, bottomNeighbourId: response.data.bottomNeighbourId, leftNeighbourId:response.data.leftNeighbourId, rightNeighbourId:response.data.rightNeighbourId, background: myimage};
       visitedNodes.push(mapdata);
       var toVisit = [
-        {id: topNeighbourId, x:mapdata.x, y:mapdata.y-1},
-        {id: bottomNeighbourId, x:mapdata.x, y:mapdata.y+1},
-        {id: rightNeighbourId, x:mapdata.x+1, y:mapdata.y}
+        {id: response.data.topNeighbourId, x:mapdata.x, y:mapdata.y-1},
+        {id: response.data.bottomNeighbourId, x:mapdata.x, y:mapdata.y+1},
+        {id: response.data.rightNeighbourId, x:mapdata.x+1, y:mapdata.y}
       ];
-      // ADD IF NO DUPLICATES
       checkAndAddArr(toVisit);
-      currentNode = {id: leftNeighbourId, x:mapdata.x-1, y:mapdata.y};
+      // ADD IF NO DUPLICATES
+      console.log("visitedNodes: "+visitedNodes.length+"  ---  tovisitNodes: "+tovisitNodes.length);
+      //process.stdout.clearLine();  // clear current text
+      //process.stdout.cursorTo(0);  // move cursor to beginning of line
+      //i = visitedNodes.length;
+      //var dots = new Array(i + 1).join(".");
+      //process.stdout.write("Waiting" + dots + "\n");  // write text
+      currentNode = {id: response.data.leftNeighbourId, x:mapdata.x-1, y:mapdata.y};
       previousNode = mapdata;
       resolve();
     })
@@ -68,7 +85,7 @@ function visitNode(){
         currentNode = tovisitNodes[0];
         tovisitNodes.shift();
         // GO TO NEXT TOVISITNODE
-        console.log("Hit a wall");
+        console.log("--HIT A WALL");
         resolve();
         //reject(error);
       }
@@ -87,7 +104,7 @@ function checkAndAddArr(toAdd) {
 function remove_duplicates(a, b) {
     for (var i = 0, len = a.length; i < len; i++) {
         for (var j = 0, len2 = b.length; j < len2; j++) {
-            if (a[i].name === b[j].name) {
+            if (a[i].id === b[j].id) {
                 b.splice(j, 1);
                 len2=b.length;
             }
