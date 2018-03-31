5fd01ebb3c374c320627ace82ee225f74e98d5b9
crimson-med <mederic.burlet@gmail.com>
2018-04-01 02:10:27 +0800

    Correct mapping

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Dofus-Discovery
 A tool to generate an overview of the Dofus world map and create a pathfinder algorithm
+
+# Done
+
+ - Collecting all Map IDs
+ - Collecting Maps Positions
+![Example of progress](images/progress.gif)
+
+# To Do
+
+ - Save maps to DB
+ - Implement algorithm to check if can cross a map
+ - Implement A* algorithm
--- handlers/mapHandler.js
+++ handlers/mapHandler.js
@@ -39,6 +39,7 @@ module.exports = {
   },
 
   getMapData: async function (mapId) {
+    const data = "https://proxyconnection.touch.dofus.com/data/map?lang=fr&v=2.19.0b"
     return axios.get(`${ASSETS_ROOT_URL}/maps/${mapId}.json`)
       .then(response => {
         return {
@@ -52,46 +53,52 @@ module.exports = {
         }
       })
       .catch(() => null);
+  },
+
+  getAllMapsId: async function () {
+    const data = "https://proxyconnection.touch.dofus.com/data/map?lang=fr&v=2.19.0b"
+    return axios.post(`${data}`, {class:"MapCoordinates",ids:[]})
+      .then(response => {
+          var allMaps = [];
+          console.log(Object.values(response.data).length);
+          for (var i = 0; i < Object.values(response.data).length; i++) {
+            allMaps.concat(Object.values(response.data)[i].mapIds);
+          }
+          return allMaps = a.filter(function(item, pos, self) {
+            return self.indexOf(item) == pos;
+          })
+      })
+      .catch(error => {
+      console.log(error);});
+  },
+  getAllMapsId: async function (mapIds) {
+    const data = "https://proxyconnection.touch.dofus.com/data/map?lang=fr&v=2.19.0b"
+    return axios.post(`${data}`, {class:"MapPositions",ids:mapIds})
+      .then(response => {
+          var allMapsData = [];
+          const totalMaps = Object.values(response.data).length;
+          for (var i = 0; i < Object.values(response.data).length; i++) {
+            var mapData = {mapID: Object.values(response.data)[i].id, posX: Object.values(response.data)[i].posX, posY: Object.values(response.data)[i].posY}
+
+            process.stdout.clearLine();  // clear current text
+            process.stdout.cursorTo(0);  // move cursor to beginning of line
+            console.log(mapData);
+            var percent = ((i+1)/totalMaps*50).toFixed(2) ;
+            var progress = "";
+            for (var j = 0; j < 50; j++) {
+              if (j <= percent) {
+                progress = progress + "=";
+              }else{
+                progress = progress + "-";
+              }
+            }
+            progress = "[" + progress + "]";
+            process.stdout.write(progress + "   map "+i+"/"+totalMaps);
+            allMapsData.push(mapData);
+          }
+          return allMaps
+      })
+      .catch(error => {
+      console.log(error);});
   }
 };
-
-
-async function getAllMaps(startMapId, maxNumberOfMaps) {
-  // Queue of map ids awaiting to be visited (breadth-first search)
-  let pendingQueue = [startMapId];
-  // List of maps that have been visited
-  let visited = [];
-
-  while (pendingQueue.length > 0 && visited.length < maxNumberOfMaps) {
-    // Pop a map id from the top of the queue
-    const headMapId = pendingQueue.shift();
-    // Fetch the data for that map
-    const mapData = await getMapData(headMapId);
-    console.log(mapData);
-    if (mapData !== null) {
-      mapData.neighbourIds.forEach(neighbourId => {
-        if (pendingQueue.indexOf(neighbourId) === -1 && !visited.some(m => m.id == neighbourId)) {
-          pendingQueue.push(neighbourId);
-        }
-      });
-      visited.push(mapData);
-    }
-  }
-  return visited;
-}
-
-async function getMapData(mapId) {
-  return axios.get(`${ASSETS_ROOT_URL}/maps/${mapId}.json`)
-    .then(response => {
-      return {
-        ...response.data,
-        neighbourIds: [
-          response.data.leftNeighbourId,
-          response.data.rightNeighbourId,
-          response.data.topNeighbourId,
-          response.data.bottomNeighbourId
-        ]
-      }
-    })
-    .catch(() => null);
-}
