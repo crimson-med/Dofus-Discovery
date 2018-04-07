284422332d067c0a0de455989cd312a698b18283
crimson-med <mederic.burlet@gmail.com>
2018-04-07 17:36:34 +0800

    Revised downloader work

--- app.js
+++ app.js
@@ -76,12 +76,3 @@ mapHandler.getAllMaps('88212247', 100)
 });*/
 
 module.exports = app;
-
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
-
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
-};
--- handlers/mapDownloader.js
+++ handlers/mapDownloader.js
@@ -1,5 +1,34 @@
+var fs = require('fs');
+request = require('request');
+
 function downloadMaps(allMaps){
   for (var i = 0; i < allMaps.length; i++) {
-    download(allMaps[i]);
+    allMaps[i]
   }
+  downloadAllImages(mapsToRender.maps)
+  .then(function (allImages) {
+    console.log("All images are loaded!", allImages); // [Img, Img, Img]
+    draw(allImages);
+  });
 }
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+var downloadAllImages = function (tiles) {
+  var totalImg = tiles.length-1;
+  var currentImg = 0;
+  return Promise.all(
+    tiles.map(function (t) {
+      return new Promise(function (resolve) {
+            resolve({background: img2, x: posX, y: posY});
+      });
+    })
+  );
+};
