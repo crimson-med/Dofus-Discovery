027c5f46611b365602c347e69e5d39722feecc06
crimson-med <mederic.burlet@gmail.com>
2018-04-11 03:31:17 +0800

    more debug

--- handlers/mapDownloader.js
+++ handlers/mapDownloader.js
@@ -27,6 +27,7 @@ async function downloadImage(map) {
     responseType: 'arraybuffer',
     url: url_dl,
     method: 'get',
+    timeout: 100000,
     headers: {
       'Content-Type': 'image/jpeg',
       'User-Agent': 'Mozilla/5.0 (Linux; Android 7.0; SM-G930VC Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/58.0.3029.83 Mobile Safari/537.36',
@@ -44,7 +45,10 @@ async function downloadImage(map) {
       });
     console.log("downloaded at: " + outputFilename);
       resolve(outputFilename);
-    });
+    })
+    .catch(error => {
+    console.log(error)
+});
 
   });
 }
